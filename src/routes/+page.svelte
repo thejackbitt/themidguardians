d41be8259604bbb54bfb32af63255f123037f39e
Jack Bittner <jack.bittner@outlook.com>
2026-02-02 22:46:10 -0600

    feat: initial code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<style>
+
+    @font-face {
+    font-family: 'Mustasurma';
+    src: url('$lib/assets/mustasurma.regular.ttf') format('ttf');
+    }
+
+    h1, h2, h3 {
+        font-family: 'Mustasurma';
+        font-weight: 100;
+        color: white;
+    }
+
+    h1 {
+        font-size: 3em;
+    }
+
+    h2 {
+        margin-right: 10px;
+    }
+
+    h3 {
+        display: flex;
+        align-items: end;
+        height: 70px;
+        margin-left: 10px;
+    }
+
+    p {
+        color: white;
+    }
+
+    a {
+        color: #6798e0;
+    }
+
+    .logo-container {
+        display: flex;
+    }
+
+    .content {
+        width: 90%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    hr.solid {
+        border-top: 3px solid white;
+        margin-top: 0.5em;
+        margin-bottom: 0.5em;
+        width: 50%;
+    }
+
+    .subhead {
+        margin-top: -10px;
+    }
+
+</style>
+<div class="content">
+    <div class="logo-container">
+        <h2>The</h2><h1>Midguardians</h1><h3>.com</h3>
+    </div>
+    <p class="subhead"><i>"What you've seen is only the tip of the iceberg."</i></p>
+    <hr class="solid">
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
