b0ef447f84264655a7d3a15b7b4870651c1743c8
Jack Bittner <jack.bittner@outlook.com>
2026-02-17 17:19:35 -0600

    feat: made comic page mobile-friendly

--- src/lib/components/Comic.svelte
+++ src/lib/components/Comic.svelte
@@ -1,4 +1,6 @@
 <script>
+  import alert from "$lib/assets/msg_warning-0.png"
+    import { onMount } from "svelte";
   /** @type {Record<string, string>} */
   const pages = import.meta.glob('$lib/assets/pages/*.png', {
     eager: true,
@@ -19,6 +21,11 @@
     .map(([, url]) => /** @type {string} */ (url));
 
   const totalPages = pagesArr.length;
+  let showAlert = $state(false);
+  /**
+     * @type {HTMLDivElement}
+     */
+  let closable;
   let pageState = $state(1);
 
   /**
@@ -37,9 +44,37 @@
     }
     return 0;
   }
+
+  /**
+     * @param {MouseEvent & { currentTarget: EventTarget & HTMLButtonElement; }} e
+     */
+  function close(e) {
+    if(closable) {
+        showAlert = false;
+    }
+    return 0;
+  }
+
+  onMount(() => {
+    if(window.innerWidth < 768) {
+        setTimeout(() => {
+            showAlert = true;
+        }, 500);
+    }
+  })
 </script>
 
 <style>
+    @keyframes alertAnim {
+        0%, 100% {
+            transform: translateY(0);
+        }
+
+        10% {
+            transform: translateY(-8px);
+        }
+    }
+    
     .comic-viewer {
         padding: 15px 0px;
         max-width: 600px;
@@ -60,22 +95,54 @@
     .field-row {
         font-size: 15px;
     }
+
+    .notification {
+        position: absolute;
+        top: 270px;
+        animation: alertAnim 0.5s ease 0s 1 normal forwards;
+    }
 </style>
 
-<div class="window" style="margin: 32px; width: 600px">
+{#if showAlert}
+    <div bind:this={closable} class="window window-box notification" style="margin: 32px;">
+        <div class="title-bar">
+            <div class="title-bar-text">
+                Notice
+            </div>
+            <div class="title-bar-controls">
+                <button on:click={(event) => close(event)} aria-label="Close"></button>
+            </div>
+        </div>
+    <div class="window-body">
+        <div class="window-alert">
+            <div class="window-alert-img">
+                <img class="image" src={alert}>
+            </div>
+            <p>It looks like you're on a mobile device.<br/><br/>  For a better viewing experience, try visiting this webpage on a desktop computer.
+        </div>
+        <div class="centered-button-container">
+            <button on:click={(event) => close(event)}>Dismiss</button>
+        </div>
+    </div>
+    </div>
+{/if}
+
+<div class="window window-box" style="margin: 32px;">
     <div class="title-bar">
       <div class="title-bar-text">
         Issue #1
       </div>
     </div>
-    <div class="centered-button-container">
-        <button on:click={(event) => trySetPageState(event, pageState-1)}>&lt;= Prev</button>
-        <div class="field-row">
-            <input id="text17" type="text" value={pageState} on:change={(event) => trySetPageState(event)}/>
+    <div class="window-body">
+        <div class="centered-button-container">
+            <button on:click={(event) => trySetPageState(event, pageState-1)}>&lt;= Prev</button>
+            <div class="field-row">
+                <input id="text17" style="width: 70px; font-size: 15px;" type="text" value={pageState} on:change={(event) => trySetPageState(event)}/>
+            </div>
+            <button on:click={(event) => trySetPageState(event, pageState+1)}>Next =&gt;</button>
         </div>
-        <button on:click={(event) => trySetPageState(event, pageState+1)}>Next =&gt;</button>
-    </div>
-    <div class="comic-viewer">
-          <img src={pagesArr[pageState-1]} alt={`Comic page ${pageState}`} loading="lazy" />
+        <div class="comic-viewer">
+            <img src={pagesArr[pageState-1]} alt={`Comic page ${pageState}`} loading="lazy" />
+        </div>
     </div>
 </div>
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -7,26 +7,5 @@
 
 <div class="content">
     <Header/>
-
     <Comic/>
-    <!-- <div class="window" style="margin: 32px; width: 600px">
-    <div class="title-bar">
-      <div class="title-bar-text">
-        Who We Are
-      </div>
-    </div>
-    <div class="window-body">
-        <div class="window-flex">
-            <img class="image" src={midguardians}>
-            <p>We are the descendants of the Vinlanders that came to America in the 9th century.  We seek to protect humanity from the children of Loki.  And we will never back down.  Our mission is to expose the truth about this world by any means necessary.
-            <br/>
-            <br/>
-            To report an instance of paranormal phenomena, please visit our Contact page.
-            </p>
-        </div>
-        <div class="centered-button-container">
-            <button on:click={(event) => navigateTo('/contact')}>Send Report</button>
-        </div>
-    </div>
-    </div> -->
 </div>
